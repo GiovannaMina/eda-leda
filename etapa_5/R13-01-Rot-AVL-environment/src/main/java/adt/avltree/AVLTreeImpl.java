3f0a2a98894ed48f5081a0ccf87811cb1bce8cf2
GiovannaMina <ggigifmina@gmail.com>
2026-03-06 22:52:45 -0300

    AVL update

--- etapa_5/R13-01-Rot-AVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
+++ etapa_5/R13-01-Rot-AVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -26,21 +26,122 @@ public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements
 	// TODO Do not forget: you must override the methods insert and remove
 	// conveniently.
 
+	@Override
+   	public void insert(T element) {
+       if (element != null) {
+        	insert(this.root, element);
+       }
+   	}
+
+   	private void insert(BSTNode<T> node, T element) {
+       	if (node.isEmpty() &&) {
+           	node.setData(element);
+           	BSTNode<T> left = new BSTNode<>();
+           	node.setLeft(left);
+           	BSTNode<T> right = new BSTNode<>();
+           	node.setRight(right);
+           	left.setParent(node);
+           	right.setParent(node);
+       	} else {
+           	if (element.compareTo(node.getData()) < 0) {
+               	insert((BSTNode<T>) node.getLeft(), element);
+           	} else if (element.compareTo(node.getData()) > 0) {
+               	insert((BSTNode<T>) node.getRight(), element);
+           	}
+           	this.rebalance(node);
+       	}
+   	}
+
+	@Override
+   	public void remove(T element) {
+       	BSTNode<T> node = this.search(element);
+       	if (!node.isEmpty() && element != null) {
+           	if (node.getLeft().isEmpty() && node.getRight().isEmpty()) {
+               	node.setData(null);
+				node.setLeft(null);
+				node.setRight(null);
+               	this.rebalanceUp(node);
+           	} else if ((!node.getLeft().isEmpty() && node.getRight().isEmpty()) || (node.getLeft().isEmpty() && !node.getRight().isEmpty())) {
+               	if (!node.getParent().isEmpty()) {
+                   	if (node == node.getParent().getLeft()) {
+                       	if (!node.getLeft().isEmpty()) {
+                           	node.getParent().setLeft(node.getLeft());
+                           	node.getLeft().setParent(node.getParent());
+                       	} else {
+                           	node.getParent().setLeft(node.getRight());
+                           	node.getRight().setParent(node.getParent());
+                       	}
+                   	} else {
+                       	if (!node.getLeft().isEmpty()) {
+                           	node.getParent().setRight(node.getLeft());
+                           	node.getLeft().setParent(node.getParent());
+                       	} else {
+                           	node.getParent().setRight(node.getRight());
+                           	node.getRight().setParent(node.getParent());
+                       	}
+                   	}
+					this.rebalanceUp(node);
+               	} else {
+                   	if (!node.getLeft().isEmpty()) {
+						this.root.getLeft().setParent(null);
+                       	this.root = (BSTNode<T>) node.getLeft();
+                   	} else {
+						this.root.getRight().setParent(null);
+                       	this.root = (BSTNode<T>) node.getRight();
+                   	}
+               	}
+
+
+           	} else {
+               	BSTNode<T> sucessor = this.sucessor(node.getData());
+               	T sucessorData = sucessor.getData();
+               	remove(sucessor.getData());
+               	node.setData(sucessorData);
+           	}
+       	}
+   	}
+
 	// AUXILIARY
 	protected int calculateBalance(BSTNode<T> node) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int answer = 0;
+		if (!node.isEmpty()) {
+			answer = this.height((BSTNode<T>) node.getLeft()) - this.height((BSTNode<T>) node.getRight());
+		}
+		return answer;
 	}
 
 	// AUXILIARY
 	protected void rebalance(BSTNode<T> node) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!node != null || !node.isEmpty()) {
+			int balance = calculateBalance(node);
+       		if (balance > 1) {
+           		int leftBalance = calculateBalance((BSTNode<T>) node.getLeft());
+           		if (leftBalance >= 0) {
+               		rotateRight(node);
+           		} else {
+               		rotateLeft((BSTNode<T>) node.getLeft());
+               		rotateRight(node);
+           		}
+       		} else if (balance < -1) {
+           		int rightBalance = calculateBalance((BSTNode<T>) node.getRight());
+           		if (rightBalance <= 0) {
+               		rotateLeft(node);
+           		} else {
+               		rotateRight((BSTNode<T>) node.getRight());
+               		rotateLeft(node);
+           		}
+       		}
+   		}
 	}
+
 
 	// AUXILIARY
 	protected void rebalanceUp(BSTNode<T> node) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+		BSTNode<T> parent = (BSTNode<T>) node.getParent();
+		if (parent != null && !parent.isEmpty()) {
+			rebalance((BSTNode<T>) parent);
+			rebalanceUp((BSTNode<T>) parent);
+		}
+   	}
+
 }
--- etapa_5/R13-01-Rot-AVL-environment/src/main/java/adt/bst/BSTImpl.java
+++ etapa_5/R13-01-Rot-AVL-environment/src/main/java/adt/bst/BSTImpl.java
@@ -1,236 +1,297 @@
 package adt.bst;
 
+
+import java.util.LinkedList;
+
+
 public class BSTImpl<T extends Comparable<T>> implements BST<T> {
 
-	protected BSTNode<T> root;
 
-	public BSTImpl() {
-		root = new BSTNode<T>();
-	} 
+   protected BSTNode<T> root;
 
-	public BSTNode<T> getRoot() {
-		return this.root;
-	}
 
-	@Override
-	public boolean isEmpty() {
-		return root.isEmpty();
-	}
+   public BSTImpl() {
+       root = new BSTNode<T>();
+   }
 
-	@Override
-	public int height() {
-		return height(this.root);
-	}
 
-	private int height(BSTNode<T> node) {
-		int result = -1;
-		if (!node.isEmpty()) {
+   public BSTNode<T> getRoot() {
+       return this.root;
+   }
 
-			// Para saber a altura de forma eficiente, precisamos saber em que lado da BST está a folha de
-			// maior nível. Por esse motivo, utilizamos Math.max, comparamos a sub-árvore da ESQUERDA com
-			// a da DIREITA para descobrir em qual lado está o maior nível.
 
-			result = 1 + Math.max(height((BSTNode<T>) node.getLeft()), height((BSTNode<T> node.getRight())));
-		}
-		return result
-	}
+   @Override
+   public boolean isEmpty() {
+       return root.isEmpty();
+   }
 
-	@Override
-	public BSTNode<T> search(T element) {
-		BSTNode<T> result = null;
-		if (element != null) {
-			result = search(this.root, element);
-		}
-		return result;
-	}
 
-	private BSTNode<T> search(BSTNode<T> node, T element) {
-		if (!node.isEmpty()) {
-			if (!node.getData().equals(element) == 0) {
-				if (element.compareTo(node.getData()) > 0) {
-					search((BSTNode<T>) node.getRight(), element);
-				} else {
-					search((BSTNode<T>) node.getLeft(), element);
-				}
-			}
-		}
-		return node;
-	}
+   	@Override
+   	public int height() {
+       return height(this.root);
+   	}
 
-	@Override
-	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
 
-	@Override
-	public BSTNode<T> maximum() {
-		return maximum(this.root);
-	}
+   private int height(BSTNode<T> node) {
+       int height = -1;
+       if (!node.isEmpty()) {
+           height = 1 + Math.max(height((BSTNode<T>)node.getLeft()), height((BSTNode<T>)node.getRight()));
+       }
+       return height;
+   }
 
-	private BSTNode<T> maximum(BSTNode<T> node) {
-		BSTNode<T> result = null;
-		if (!node.isEmpty()) {
-			if (!node.getRight().isEmpty()) {
-				result = this.maximum((BSTNode<T>) node.getRight());
-			}
-			else {
-				result = node;
-			}
-		}
-		return result;
-	}
 
-	@Override
-	public BSTNode<T> minimum() {
-		return minimum(this.root);
-	}
+   @Override
+   public BSTNode<T> search(T element) {
+       return search(this.root, element);
+   }
+    private BSTNode<T> search(BSTNode<T> node, T element) {
+       BSTNode<T> result = new BSTNode<>();
+       if (node.isEmpty() || element.equals(node.getData())) {
+           result = node;
+       }
+       else if (element.compareTo(node.getData()) < 0) {
+           result = search((BSTNode<T>)node.getLeft(), element);
+       }
+       else {
+           result = search((BSTNode<T>)node.getRight(), element);
+       }
+       return result;
+   }
 
-	private BSTNode<T> minimum(BSTNode<T> node) {
-		BSTNode<T> result = null;
-		if (!node.isEmpty()) {
-			if (!node.getLeft().isEmpty()) {
-				result = this.minimum((BSTNode<T>) node.getLeft());
-			}
-			else {
-				result = node;
-			}
-		}
-		return result;
-	}
 
-	@Override
-	public BSTNode<T> sucessor(T element) {
-		BSTNode<T> result = null;
-		if (element != null) {
 
-			// Nao precisa do elemento, e sim do nó, por isso é necessário buscar o nó na árvore
-			// para poder buscar o sucessor
 
-			result = sucessor(search(element));
-		}
-		return result;
-	}
+   @Override
+   public void insert(T element) {
+       if (element != null) {
+           insert(this.root, element);
+       }
+   }
+  
+   private void insert(BSTNode<T> node, T element) {
+       if (node.isEmpty()) {
+           node.setData(element);
+           BSTNode<T> left = new BSTNode<>();
+           node.setLeft(left);
+           BSTNode<T> right = new BSTNode<>();
+           node.setRight(right);
+           left.setParent(node);
+           right.setParent(node);
+       }
+       else {
+           if (element.compareTo(node.getData()) < 0) {
+               insert((BSTNode<T>) node.getLeft(), element);
+           }
+           else if (element.compareTo(node.getData()) > 0) {
+               insert((BSTNode<T>) node.getRight(), element);
+           }
+       }
+   }
 
-	// O sucessor ele geralmente será o nó que está à direita do nó x -> Porém
-	// há a possibilidade desse nó possuir um nó a esquerda, ou seja
-	// um nó menor que ele, logo o correto seria procurar o valor MíNIMO da sub-árvore. 
-	// Entretanto, há a possibilidade do nó x não ter um filho a direita, quando isso acontece, 
-	// quer dizer que o sucessor é um nó que está acima do nó x na hierarquia.
-	// "Mas como eu vou saber qual nó é esse? Qual parente é?" Bem, vamos verificar os nós filho
-	// desse parente. Se o nó x for o nó filho a direita do parente, então o nó x é maior que o seu
-	// pai, dessa forma, seu pai não pode ser seu sucessor. Se o nó x estiver à esquerda do parente,
-	// então ele é menor que seu pai, logo, seu pai é o seu sucessor (isso se o nó x não tiver um filho
-	// a direita).
 
-	private BSTNode<T> sucessor(BSTNode<T> node) {
-		BSTNode<T> result = null;
+   @Override
+   public BSTNode<T> maximum() {
+       return maximum(this.root);
+   }
+    private BSTNode<T> maximum(BSTNode<T> node) {
+       BSTNode<T> result = null;
+       if (!node.isEmpty()) {
+           BSTNode<T> nextMax = this.maximum((BSTNode<T>)node.getRight());
+           if (nextMax != null && !nextMax.isEmpty()) {
+               result = nextMax;
+           }
+           else {
+               result = node;
+           }
+       }
+       return result;
+   }
 
-		// O que acontece se o nó filho a DIREITA, estiver vazio? A chance desse nó a DIREITA ser o 
-		// sucessor existe, entretando, isso em um caso positivo que esse nó a DIREITA não possua um nó
-		// a ESQUERDA. Por isso, é necessário que buscamos não o nó a direita, e sim o valor mínimo daquela
-		// sub-árvore, sendo o nó a direita sua raíz.
 
-		if (!node.getRight().isEmpty()) {
+   @Override
+   public BSTNode<T> minimum() {
+       return this.minimum(this.root);
+   }
+    private BSTNode<T> minimum(BSTNode<T> no) {
+       BSTNode<T> result = null;
+       if (!no.isEmpty()) {
+           BSTNode<T> nextMin = this.minimum((BSTNode<T>)no.getLeft());
+           if (nextMin != null && !nextMin.isEmpty()) {
+               result = nextMin;
+           } else {
+               result = no;
+           }
+       }
+       return result;
+   }
 
-			// Código anterior: result = node.getRight();
 
-			result = minimo(node.getRight());
-		}
+   @Override
+   public BSTNode<T> sucessor(T element) {
+       BSTNode<T> result = null;
+       BSTNode<T> node = search(element);
+       if (!node.isEmpty()) {
+           result = sucessor(node);
+       }
+       return result;
+      
+   }
+   private BSTNode<T> sucessor(BSTNode<T> node) {
+       BSTNode<T> result = null;
+       if (!node.getRight().isEmpty()) {
+           result = minimum((BSTNode<T>)node.getRight());
+       } else {
+           BSTNode<T> parent = (BSTNode<T>)node.getParent();
+           while (parent != null && node == parent.getRight()) {
+               node = parent;
+               parent = (BSTNode<T>)parent.getParent();
+           }
+           result = parent;
+       }
+       return result;
+   }
 
-		// Se o nó a DIREITA estiver vazio, quer dizer que o sucessor vai estar nos nós que estão antes
-		// do nó x. Para saber qual parente é esse, precisamos levar dois casos em consideração.
-		// Caso 1 - O nó x é o nó a DIREITA do seu pai: se o nó x for o nó a direita do seu pai,
-		// então quer dizer que o nó x é maior que seu pai, não podendo ser ele seu sucessor, logo
-		// subimos a comparação para o AVÔ.
-		// Caso 2 - O nó x é o nó a ESQUERDA do seu pai: Se isso acontecer, e o nó x não possuir um filho
-		// a direita, então quer dizer que o nó sucessor de x, é o seu próprio PAI.
 
-		else {
-			BSTNode<T> parent = (BSTNode<T>) node.getParent();
+   @Override  
+   public BSTNode<T> predecessor(T element) {
+       BSTNode<T> result = null;
+       BSTNode<T> node = search(element);
+       if (!node.isEmpty()) {
+           result = predecessor(node);
+       }
+       return result;
+   }
+   private BSTNode<T> predecessor(BSTNode<T> node) {
+       BSTNode<T> result = null;
+       if (!node.getLeft().isEmpty()) {
+           result = this.maximum((BSTNode<T>)node.getLeft());
+       } else {
+           BSTNode<T> pai = (BSTNode<T>)node.getParent();
+           while (pai != null && node == pai.getLeft()) {
+               node = pai;
+               pai = (BSTNode<T>)pai.getParent();
+           }
+           result = pai;
+       }
+       return result;
+   }
 
-			// "Enquanto eu não chegar no topo da árvore (pai não é nulo) 
-			// E o nó atual for o filho à direita do seu pai, continue subindo."
 
-			while (!parent != null && parent.getRight().equals(node)) {
-				node = parent;
-				parent = (BSTNode<T>) parent.getParent();
-			}
+   @Override
+   public void remove(T element) {
+       BSTNode<T> node = this.search(element);
+       if (!node.isEmpty()) {
+           if (node.getLeft().isEmpty() && node.getRight().isEmpty()) {
+               node.setData(null);
+           } else if ((!node.getLeft().isEmpty() && node.getRight().isEmpty()) || (node.getLeft().isEmpty() && !node.getRight().isEmpty())) {
+               if (!node.getParent().isEmpty()) {
+                   if (node == node.getParent().getLeft()) {
+                       if (!node.getLeft().isEmpty()) {
+                           node.getParent().setLeft(node.getLeft());
+                           node.getLeft().setParent(node.getParent());
+                       } else {
+                           node.getParent().setLeft(node.getRight());
+                           node.getRight().setParent(node.getParent());
+                       }
+                   } else {
+                       if (!node.getLeft().isEmpty()) {
+                           node.getParent().setRight(node.getLeft());
+                           node.getLeft().setParent(node.getParent());
+                       } else {
+                           node.getParent().setRight(node.getRight());
+                           node.getRight().setParent(node.getParent());
+                       }
+                   }
+               } else {
+                   if (!node.getLeft().isEmpty()) {
+                       this.root = (BSTNode<T>)node.getLeft();
+                       this.root.setParent(null);
+                   } else {
+                       this.root = (BSTNode<T>)node.getRight();
+                       this.root.setParent(null);
+                   }
+               }
+           } else {
+               BSTNode<T> sucessor = this.sucessor(node);
+               T sucessorData = sucessor.getData();
+               remove(sucessor.getData());
+               node.setData(sucessorData);
+           }
+       }
+   }
 
-			// Se o pai chegar a ser nulo (chegamos na raíz), quer dizer que chegamos no
-			// maior valor da árvore, e como o maior valor não tem sucessor, então o valor será nulo.
 
-			result = parent;
-		}
-		return result;
-	}
+   @Override
+   public T[] preOrder() {
+       LinkedList<T> list = new LinkedList<>();
+       preOrder(this.root, list);
+       return (T[]) list.toArray(new Comparable[this.size()]);
+   }
 
-	@Override
-	public BSTNode<T> predecessor(T element) {
-		BSTNode<T> result = null;
-		if (element != null) {
-			result = predecessor(search(element));
-		}
-		return result;
-	}
 
-	private BSTNode<T> predecessor(BSTNode<T> node) {
-		BSTNode<T> result = null;
-		if (!node.getLeft().isEmpty()) {
-			result = maximo(node.getLeft());
-		}
-		else {
-			BSTNode<T> parent = (BSTNode<T>) node.getParent();
-			while (!parent != null && parent.getLeft().equals(node)) {
-				node = parent;
-				parent = (BSTNode<T>) parent.getParent();
-			}
-			result = parent;
-		}
-		return result;
-	}
+   private void preOrder(BSTNode<T> node, LinkedList<T> list) {
+       if (!node.isEmpty()) {
+           list.add(node.getData());
+           preOrder((BSTNode<T>) node.getLeft(), list);
+           preOrder((BSTNode<T>) node.getRight(), list);
+       }
+   }
 
-	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
 
-	@Override
-	public T[] preOrder() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   @Override
+   public T[] order() {
+       LinkedList<T> list = new LinkedList<>();
+       order(this.root, list);
+       return (T[]) list.toArray(new Comparable[this.size()]);
+   }
 
-	@Override
-	public T[] order() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
 
-	@Override
-	public T[] postOrder() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   private void order (BSTNode<T> node, LinkedList<T> list) {
+       if (!node.isEmpty()) {
+           order((BSTNode<T>) node.getLeft(), list);
+           list.add(node.getData());
+           order((BSTNode<T>) node.getRight(), list);
+       }
+   }
 
-	/**
-	 * This method is already implemented using recursion. You must understand
-	 * how it work and use similar idea with the other methods.
-	 */
-	@Override
-	public int size() {
-		return size(root);
-	}
 
-	private int size(BSTNode<T> node) {
-		int result = 0;
-		// base case means doing nothing (return 0)
-		if (!node.isEmpty()) { // indusctive case
-			result = 1 + size((BSTNode<T>) node.getLeft())
-					+ size((BSTNode<T>) node.getRight());
-		}
-		return result;
-	}
+   @Override
+   public T[] postOrder() {
+       LinkedList<T> list = new LinkedList<>();
+       postOrder(this.root, list);
+       return (T[]) list.toArray(new Comparable[this.size()]);
+   }
+
+
+   private void postOrder(BSTNode<T> node, LinkedList<T> list) {
+       if (!node.isEmpty()) {
+           order((BSTNode<T>) node.getLeft(), list);
+           order((BSTNode<T>) node.getRight(), list);
+           list.add(node.getData());
+       }
+   }
+
+
+   /**
+    * This method is already implemented using recursion. You must understand
+    * how it work and use similar idea with the other methods.
+    */
+   @Override
+   public int size() {
+       return size(root);
+   }
+
+
+   private int size(BSTNode<T> node) {
+       int result = 0;
+       // base case means doing nothing (return 0)
+       if (!node.isEmpty()) { // indusctive case
+           result = 1 + size((BSTNode<T>) node.getLeft())
+                   + size((BSTNode<T>) node.getRight());
+       }
+       return result;
+   }
+
 
 }
